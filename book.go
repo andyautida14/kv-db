fa593fd0f1b0840c46a0ef3c6e46bbd5077e004b
Andy Autida <risejandiautida@gmail.com>
2023-11-01 23:36:34 +0800

    bufferize books

---
+++ book.go
@@ -0,0 +1,84 @@
+package main
+
+import "encoding/binary"
+
+const BookSize = (4 * 32) + 4
+
+type Book struct {
+	Title     string
+	PageCount uint32
+}
+
+type BookBuffer struct {
+	buf [(4 * 32) + 4]byte
+}
+
+func (b *BookBuffer) Title() string {
+	return string(b.buf[:(4*32)-1])
+}
+
+func (b *BookBuffer) SetTitle(v string) {
+	copy(b.buf[:(4*32)-1], []byte(v))
+}
+
+func (b *BookBuffer) PageCount() uint32 {
+	return binary.LittleEndian.Uint32(b.buf[4*32:])
+}
+
+func (b *BookBuffer) SetPageCount(v uint32) {
+	binary.LittleEndian.PutUint32(b.buf[4*32:], v)
+}
+
+func (b *BookBuffer) ToBook() *Book {
+	return &Book{
+		Title:     b.Title(),
+		PageCount: b.PageCount(),
+	}
+}
+
+func (b *BookBuffer) SetFromBook(book *Book) {
+	b.SetTitle(book.Title)
+	b.SetPageCount(book.PageCount)
+}
+
+func (b *BookBuffer) ToWriterAt(w writerAt, off int) (int, error) {
+	off_b := int64(BookSize * off)
+	return w.WriteAt(b.buf[:], off_b)
+}
+
+func (b *BookBuffer) FromReaderAt(r readerAt, off int) (int, error) {
+	off_b := int64(BookSize * off)
+	return r.ReadAt(b.buf[:], off_b)
+}
+
+func NewBookBufferFromBook(b *Book) *BookBuffer {
+	bb := BookBuffer{}
+	bb.SetTitle(b.Title)
+	bb.SetPageCount(b.PageCount)
+	return &bb
+}
+
+func NewBookBufferFromReaderAt(r readerAt, off int) (*BookBuffer, error) {
+	off_b := int64(BookSize * off)
+	bb := BookBuffer{}
+	if _, err := r.ReadAt(bb.buf[:], off_b); err != nil {
+		return nil, err
+	}
+	return &bb, nil
+}
+
+func TotalBookCount(s fileStat) int64 {
+	return s.Size() / BookSize
+}
+
+type readerAt interface {
+	ReadAt(p []byte, off int64) (n int, err error)
+}
+
+type writerAt interface {
+	WriteAt(p []byte, off int64) (n int, err error)
+}
+
+type fileStat interface {
+	Size() int64
+}
--- main.go
+++ main.go
@@ -1,64 +1,24 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
 	"log"
 	"os"
 )
 
-const BOOK_TITLE_S = 4 * 32
-const BOOK_S = BOOK_TITLE_S + 4
-
-type Book struct {
-	Title     string
-	PageCount uint32
-}
-
-func writeBook(f *os.File, book *Book, off int) error {
-	off_b := int64(BOOK_S * off)
-
-	title_b := make([]byte, BOOK_TITLE_S)
-	copy(title_b, []byte(book.Title))
-	if _, err := f.WriteAt(title_b, off_b); err != nil {
-		return err
-	}
-
-	pageCount_b := make([]byte, 4)
-	binary.LittleEndian.PutUint32(pageCount_b, book.PageCount)
-	_, err := f.WriteAt(pageCount_b, BOOK_TITLE_S+off_b)
-	return err
-}
-
-func readBook(f *os.File, book *Book, off int) error {
-	off_b := int64(BOOK_S * off)
-
-	title_b := make([]byte, BOOK_TITLE_S)
-	if _, err := f.ReadAt(title_b, off_b); err != nil {
-		return err
-	}
-
-	pageCount_b := make([]byte, 4)
-	if _, err := f.ReadAt(pageCount_b, BOOK_TITLE_S+off_b); err != nil {
-		return err
-	}
-
-	book.Title = string(title_b)
-	book.PageCount = binary.LittleEndian.Uint32(pageCount_b)
-
-	return nil
-}
-
-func writeBooks(books *[]Book) error {
+func writeBooks(books []Book) error {
 	f, err := os.OpenFile("./data/data", os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
-	for i := 0; i < len(*books); i++ {
-		if err := writeBook(f, &(*books)[i], i); err != nil {
-			log.Fatal(err)
+	bookBuf := BookBuffer{}
+
+	for i := 0; i < len(books); i++ {
+		bookBuf.SetFromBook(&books[i])
+		if _, err := bookBuf.ToWriterAt(f, i); err != nil {
+			return err
 		}
 	}
 
@@ -76,16 +36,16 @@ func readBooks() (*[]Book, error) {
 	if err != nil {
 		return nil, err
 	}
-	booksCount := stat.Size() / BOOK_S
+	booksCount := TotalBookCount(stat)
+
+	bookBuf := BookBuffer{}
 
 	books := []Book{}
 	for i := 0; i < int(booksCount); i++ {
-		book := Book{}
-		if err := readBook(f, &book, i); err != nil {
+		if _, err := bookBuf.FromReaderAt(f, i); err != nil {
 			return nil, err
 		}
-
-		books = append(books, book)
+		books = append(books, *bookBuf.ToBook())
 	}
 
 	return &books, nil
@@ -115,7 +75,7 @@ func main() {
 		},
 	}
 
-	if err := writeBooks(&booksToWrite); err != nil {
+	if err := writeBooks(booksToWrite); err != nil {
 		log.Fatal(err)
 	}
 
